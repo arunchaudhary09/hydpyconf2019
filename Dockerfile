61b82b7adfc4b51645706ca462e97c5a0fc97230
Arun Chaudhary <chaudhar@qti.qualcomm.com>
2019-11-13 06:45:27 +0530

    remove pages stuff and add remote thingy

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,20 @@
-FROM ruby:2.4.0	
-ENV RUBYGEMS_VERSION=2.7.0
-# Set default locale for the environment	
-ENV LC_ALL C.UTF-8	
-ENV LANG en_US.UTF-8	
-ENV LANGUAGE en_US.UTF-8	
+FROM debian:stable-slim
 
-LABEL "com.github.actions.name"="Build & Deploy to GitHub Pages"	
-LABEL "com.github.actions.description"="Builds & deploys Jekyll to gh-pages branch of the same repository. "	
-LABEL "com.github.actions.icon"="globe"	
-LABEL "com.github.actions.color"="green"	
+LABEL "maintainer"="Arun Chaudhary <arunchaudhary.bit@gmail.com>"
+LABEL "repository"="https://github.com/arunchaudhary09/hydpyconf2019@ci-cd"
+LABEL "version"="1.0.1"
 
-LABEL "repository"="http://github.com/arunchaudhary09/hydpyconf2019"	
+LABEL "com.github.actions.name"="SSH"
+LABEL "com.github.actions.description"="Run command via SSH"
+LABEL "com.github.actions.icon"="server"
+LABEL "com.github.actions.color"="orange"
+
+RUN apt-get update && apt-get install -y \
+  openssh-client && \
+  rm -Rf /var/lib/apt/lists/*
+
 
 ADD entrypoint.sh /entrypoint.sh
+
 
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,24 +1,21 @@
-#!/bin/sh 
+#!/bin/sh
 
-set -x
+set -e
 
-echo '👍 ENTRYPOINT HAS STARTED—INSTALLING THE GEM BUNDLE'
-bundle install
-bundle list | grep "jekyll ("
-echo '👍 BUNDLE INSTALLED—BUILDING THE SITE'
-bundle exec jekyll build -d build
-echo '👍 THE SITE IS BUILT—PUSHING IT BACK TO GITHUB-PAGES'
-ls -al
-cd build 
-remote_repo="https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" && \
-remote_branch="test" && \
-git init && \
-git config user.name "${GITHUB_ACTOR}" && \
-git config user.email "${GITHUB_ACTOR}@users.noreply.github.com" && \
-git add . && \
-echo -n 'Files to Commit:' && ls -l | wc -l && \
-git commit -m'action build'  && \
-git push --force $remote_repo master:$remote_branch && \
-rm -fr .git && \
-cd ../
-echo '👍 GREAT SUCCESS!'
+SSH_PATH="$HOME/.ssh"
+
+mkdir -p "$SSH_PATH"
+touch "$SSH_PATH/known_hosts"
+
+echo "$KEY" > "$SSH_PATH/deploy_key"
+
+chmod 700 "$SSH_PATH"
+chmod 600 "$SSH_PATH/known_hosts"
+chmod 600 "$SSH_PATH/deploy_key"
+
+eval $(ssh-agent)
+ssh-add "$SSH_PATH/deploy_key"
+
+ssh-keyscan -t rsa $HOST >> "$SSH_PATH/known_hosts"
+
+ssh -o StrictHostKeyChecking=no -A -tt -p ${PORT:-22} $USER@$HOST "$*"
