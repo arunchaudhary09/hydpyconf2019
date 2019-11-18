2f2548dc156283f1865152a33cd0046cb5791bde
Arun Chaudhary <chaudhar@qti.qualcomm.com>
2019-11-19 00:51:49 +0530

    some more debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,7 +16,7 @@ chmod 600 "$SSH_PATH/deploy_key"
 
 eval $(ssh-agent)
 ssh-add "$SSH_PATH/deploy_key"
-
+ssh-add -L
 ssh-keyscan -t rsa ${INPUT_HOST} >> "$SSH_PATH/known_hosts"
-
-ssh -i "$SSH_PATH/deploy_key" -o StrictHostKeyChecking=no -A -tt -p 22 ${INPUT_USER}@${INPUT_HOST} "$*"
+cat "$SSH_PATH/known_hosts"
+ssh -v -i "$SSH_PATH/deploy_key" -o StrictHostKeyChecking=no -A -tt -p 22 ${INPUT_USER}@${INPUT_HOST} "$*"
